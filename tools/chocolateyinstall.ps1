891948d9ac3f8fc6e7ba450c1c0723071cb8b2f9
rogier5 <nicolas.rogier@univ-lorraine.fr>
2020-10-19 09:57:57 +0200

    format chocoinstall script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
-﻿
-$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'http://www.xm1math.net/algobox/assets/files/Algobox_1.0.2_Win_x64.msi'
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64 = 'http://www.xm1math.net/algobox/assets/files/Algobox_1.0.2_Win_x64.msi'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url64bit      = $url64
-
-  softwareName  = '.\algobox*'
-  checksum64    = '4CC5A3975AD8916ADD947C62FB617375E3477FA939A537B37BA809ADA916DE38'
-  checksumType64= 'sha256'
-
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'MSI'
+  url64bit       = $url64
+  softwareName   = 'Algobox*'
+  checksum64     = '4cc5a3975ad8916add947c62fb617375e3477fa939a537b37ba809ada916de38'
+  checksumType64 = 'sha256'
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
